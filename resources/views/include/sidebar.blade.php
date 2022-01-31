bc0960adaa026770c8dd75ee5d5fd022afce889d
Pedro4000 <P.brickley@hotmail.fr>
2022-01-31 15:50:31 +0100

    modif before mep

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -1,6 +1,13 @@
+
 <a href="{{ route('home') }}">
-	<div class="sidebar_content sticky-top text-end text-6xl lg:text-xl mt-5 lg:mt-4 md:pr-12">
-		Matthieu <br>Cauchy
-	</div>
+	@if (Route::currentRouteName() != 'album')
+		<div class="sidebar_content sticky-top text-end text-6xl lg:text-xl mt-5 lg:mt-4 md:pr-12">
+			Matthieu <br>Cauchy
+		</div>
+	@else 
+		<div class="sidebar_content sticky-top text-end text-6xl lg:text-xl mt-5 lg:mt-4 md:pr-12">
+			{{ Str::ucfirst($album->nom) }}
+		</div>
+	@endif
 </a>
 
