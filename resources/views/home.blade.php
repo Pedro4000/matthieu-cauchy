8059057f0758827c0e2bc39b9f4833247c22331b
pbrickley <pbrickley@futurenet.com>
2024-10-31 17:37:15 +0100

    set martha main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,11 @@ home
         @if($photo->cover)
         <div class="w-full lg:w-1/2 mb-7 lg:mb-0 px-3 text-center flex home-main-element lg:p-6">
           <a href="{{ route('album', ['albumName' => $album->name ]) }}" class="premiere-galerie-lien">
-            <div class="inline-block premiere_galerie flex items-center w-full"  style="background-image: url({{ isset($photo->cover) ? asset('storage/photos/' . $photo->filename) : ''  }})">
+            @if(strtolower($album->name) == 'martha')
+              <div class="inline-block premiere_galerie flex items-center w-full"  style="background-image: url({{ asset('storage/marthamain.jpeg') }})">
+            @else
+              <div class="inline-block premiere_galerie flex items-center w-full"  style="background-image: url({{ isset($photo->cover) ? asset('storage/photos/' . $photo->filename) : ''  }})">
+            @endif
             </div>
             <div class="centered-title">{{ Str::of($album->name)->upper() }}</div>
           </a>
